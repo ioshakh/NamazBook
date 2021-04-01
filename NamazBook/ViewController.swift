c52fb71a9351ca9ad0b66cac7eaf1284aaac5c93
Shakhzod <79221577+ioshakh@users.noreply.github.com>
2021-04-01 16:31:11 +0500

    Initial Commit

---
+++ NamazBook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NamazBook
+//
+//  Created by shahzod on 4/1/21.
+//  Copyright © 2021 shahzod. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
